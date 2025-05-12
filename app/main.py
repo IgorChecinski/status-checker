387b329dd6aa828cf283a1a2d5d999ec4926bdc5
Igor Chęciński <s24605@pjwstk.edu.pl>
2025-05-12 12:26:02 +0200

    Basic css

--- app/main.py
+++ app/main.py
@@ -3,10 +3,14 @@ from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi import Request
+from fastapi.staticfiles import StaticFiles
+
 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 
 
 @app.get("/", response_class=HTMLResponse)
