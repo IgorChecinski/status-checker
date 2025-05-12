42029708d3a9b5afcf299325be9c2e99bca9e259
Igor Chęciński <s24605@pjwstk.edu.pl>
2025-05-12 12:12:28 +0200

    Basic frontend

--- app/main.py
+++ app/main.py
@@ -1,20 +1,26 @@
 import httpx
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get('/')
-def root():
-    return {'status': 'ok'}
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/monitor")
 async def monitor_url(url: str):
     try:
-        # Send URL 
+        # Send a GET request to the URL
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
         
-        # Check status code
+        # Determine the status of the URL
         if response.status_code == 200:
             return {"url": url, "status": "OK"}
         else:
