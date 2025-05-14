b532bab8142d050a5eff5c295d7e31ffbc1d4481
Igor Chęciński <s24605@pjwstk.edu.pl>
2025-05-14 17:18:46 +0200

    startup update

--- infra/startup.sh
+++ infra/startup.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-apt update
-apt install -y docker.io docker-compose git
 
 
-cd /opt
-git clone https://github.com/IgorChecinski/status-checker.git
-cd devops-monitor
+if ! command -v docker &> /dev/null
+then
+    echo "Docker not found. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+fi
 
+
+if ! command -v docker-compose &> /dev/null
+then
+    echo "Docker Compose not found. Installing..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+
+cd /app
 docker-compose up -d
+
+echo "Startup script completed!"
