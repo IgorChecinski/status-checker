64aa4b8ebbc6c4faa31bc032e375db545770fd34
Igor Chęciński <s24605@pjwstk.edu.pl>
2025-06-05 14:26:25 +0200

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from app.main import app
 
 @pytest.mark.asyncio
 async def test_root_returns_200():
-    async with AsyncClient(app=app, base_url="http://www.google.com") as client:
-        response = await client.get("/")
+    async with AsyncClient(app=app, base_url="http://www.google.com") as ac:
+        response = await ac.get("/")
     assert response.status_code == 200
+
